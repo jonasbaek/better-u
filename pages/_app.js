fcdded0f89127b36910a21fd40fbc54b9aedbf2a
jonasbaek <jonasbaek@hotmail.com>
2023-01-19 17:43:39 -0300

    Add Toaster component on _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
