3b72c8e18bf6c65bfa2a01204802e4337c856e43
jonasbaek <jonasbaek@hotmail.com>
2023-01-19 01:26:46 -0300

    Add google font on _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Pacifico&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
