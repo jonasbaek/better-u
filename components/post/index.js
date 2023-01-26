9b0879cf9614783969801cd622ae40a44a8b79fa
jonasbaek <jonasbaek@hotmail.com>
2023-01-26 20:19:58 -0300

    Remove ability to add or remove friend from post component

--- components/post/index.js
+++ components/post/index.js
@@ -1,9 +1,5 @@
 import AvatarComponent from "../avatar";
-import Button from "@mui/material/Button";
-import CloseIcon from "@mui/icons-material/Close";
 import styles from "../../styles/styles.module.scss";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import PersonRemoveIcon from "@mui/icons-material/PersonRemove";
 import { IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import MenuItem from "@mui/material/MenuItem";
@@ -14,19 +10,11 @@ import Menu from "@mui/material/Menu";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import { Snackbar } from "@mui/material";
-import { useState, Fragment } from "react";
+import { useState } from "react";
 
 export default function Post(props) {
   const [anchorEl, setAnchorEl] = useState(null);
   const openSettings = Boolean(anchorEl);
-
-  const [snackBarState, setSnackBarState] = useState({
-    open: false,
-    vertical: "bottom",
-    horizontal: "right",
-  });
-  const { vertical, horizontal, open } = snackBarState;
 
   const handleClickSettings = (event) => {
     setAnchorEl(event.currentTarget);
@@ -36,25 +24,8 @@ export default function Post(props) {
     setAnchorEl(null);
   };
 
-  const handleClick = () => {
-    setSnackBarState({ ...snackBarState, open: true });
-    props.addOrRemoveFriend(props.post?.user?.id);
-  };
-
-  const handleClose = () => {
-    setSnackBarState({ ...snackBarState, open: false });
-    props.addOrRemoveFriend(props.post?.user?.id);
-  };
-
   const isCurrentUserPost = () => {
     return props.currentUser?._id === props.post?.user?.id;
-  };
-
-  const isFriendWithPostOwner = () => {
-    const friendListsFromPostOwner = props.post?.user?.friends;
-    return friendListsFromPostOwner.some(
-      (friend) => friend.user === props.currentUser?._id
-    );
   };
 
   const isPostLikedFromCurrentUser = () => {
@@ -69,24 +40,6 @@ export default function Post(props) {
     dateStyle: "full",
   });
 
-  const action = (
-    <Fragment>
-      <Button color="primary" size="small" onClick={handleClose}>
-        UNDO
-      </Button>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={() => {
-          setSnackBarState({ ...snackBarState, open: false });
-        }}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </Fragment>
-  );
-
   return (
     <section className={styles.postContainer}>
       <div className="d-flex justify-content-between">
@@ -98,15 +51,6 @@ export default function Post(props) {
           </div>
         </div>
         <div className="d-flex">
-          {!isCurrentUserPost() && (
-            <IconButton onClick={handleClick}>
-              {isFriendWithPostOwner() ? (
-                <PersonRemoveIcon />
-              ) : (
-                <PersonAddIcon />
-              )}
-            </IconButton>
-          )}
           {isCurrentUserPost() && (
             <>
               <IconButton
@@ -170,16 +114,6 @@ export default function Post(props) {
             </>
           )}
         </div>
-        <Snackbar
-          anchorOrigin={{ vertical, horizontal }}
-          open={open}
-          autoHideDuration={6000}
-          onClose={() => {
-            setSnackBarState({ ...snackBarState, open: false });
-          }}
-          message="Unfriended!"
-          action={action}
-        />
       </div>
       <p className={styles.postText}>{props.post?.text}</p>
       <div className="d-flex mt-5">
